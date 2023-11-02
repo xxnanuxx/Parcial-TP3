dc6995f1fee6bd6ea0ec7f37001dc6a62c86d561
xXNanuXx <nanu1429@gmail.com>
2023-11-02 00:22:06 -0300

    Inicio de la aplicacion, implementacion de NavGraph para LoginActiivy

---
+++ app/src/main/java/com/example/dogaplication/Fragments/WelcomeFragment.kt
@@ -0,0 +1,39 @@
+package com.example.dogaplication.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.dogaplication.R
+import com.example.dogaplication.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentWelcomeBinding
+    lateinit var btnNext : Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = FragmentWelcomeBinding.inflate(layoutInflater)
+
+        btnNext = view.findViewById<Button>(R.id.fragWelcBtnNextId)
+
+        //binding.fragWelcBtnNextId.setOnClickListener{} VERIFICAR POR QUE NO ANDA
+
+        btnNext.setOnClickListener{
+            val accion = WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment()
+            view.findNavController().navigate(accion)
+        }
+
+        return view
+    }
+
+}
