7c2f249bea0d0456b8513d34c1bc7bf18f073ef1
xXNanuXx <nanu1429@gmail.com>
2023-11-03 03:29:40 -0300

    Ya funcionan los menu en conjunto a partir de un toolbar, agregado de iconos en button bar

--- app/src/main/java/com/example/dogaplication/Activities/MainActivity.kt
+++ app/src/main/java/com/example/dogaplication/Activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dogaplication.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
@@ -16,51 +17,40 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.nav_view)
-
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         bottomNavView = findViewById(R.id.bottom_bar)
 
-        // Configurar BottomNavigationView con NavController
-        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        // Configurar el DrawerLayout
-        setupDrawerLayout()
-    }
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
 
-    private fun setupDrawerLayout() {
+        drawerLayout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.hamburger)
+
         val navController = navHostFragment.navController
 
-        // Vincular la navegación del Drawer con el gráfico de navegación
-        navigationView.setupWithNavController(navController)
+        // Configurar BottomNavigationView con NavController
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+        NavigationUI.setupWithNavController(navigationView, navController)
 
-        // Configurar la AppBar para mostrar el icono del Drawer y actualizar el título
-        //NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-
-        // Escuchar eventos de cambio de destino
-        navController.addOnDestinationChangedListener { _, _, _ ->
-            // Configurar el icono izquierdo de la AppBar como el del Drawer
-            supportActionBar?.setHomeAsUpIndicator(R.drawable.hamburger)
-        }
-        onSupportNavigateUp()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.hamburger)
-        }
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
-        return false
-    }
 }
