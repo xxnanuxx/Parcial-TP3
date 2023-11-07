2164e7e031aa293ed8d9ec3cfa19f7626896fedc
Guibert <34744071@comunidadort.edu.ar>
2023-11-07 16:53:29 -0300

    se agrega funcionalidad al boton de favoritos en el fragment_perrito_details. Cuando le haces click al boton se deberia pintar de rojo el background si lo agrega a favoritos y de blanco si se lo saca (no estaria funcionando el background color). Por otro lado cambia en la bd el favorito del perro.

--- app/src/main/java/com/example/dogaplication/fragments/PerritoDetails.kt
+++ app/src/main/java/com/example/dogaplication/fragments/PerritoDetails.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.dogaplication.R
@@ -21,6 +23,7 @@ class PerritoDetails : Fragment() {
     lateinit var dueno: TextView
     lateinit var ubicacion: TextView
     lateinit var imgView : ImageView
+    lateinit var btnFav: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +44,7 @@ class PerritoDetails : Fragment() {
         dueno = v.findViewById(R.id.txtDueno)
         ubicacion = v.findViewById(R.id.txtUbicacion)
         imgView = v.findViewById(R.id.fragPDetailImgViewId)
+        btnFav = v.findViewById(R.id.btnFavId)
 
         return v
 
@@ -59,8 +63,28 @@ class PerritoDetails : Fragment() {
                   macho.text = if (perrito?.macho == 1.toByte()) "Macho" else "Hembra"
                 dueno.text = perrito?.dueno
                 ubicacion.text = perrito?.ubicacion
+                var esFavorito = if(perrito?.favorito == 1.toByte()) true else false
+
 
                 Picasso.get().load(perrito?.imagen).into(imgView);
+
+                //pregunta si el favorito de perrito es true
+                if(esFavorito){
+                    btnFav.setBackgroundColor((R.color.colorTintRed.toInt()))
+                }
+
+                btnFav.setOnClickListener{
+                    if(esFavorito){
+                        esFavorito = false
+                        perrito?.favorito = 0.toByte()
+                        btnFav.setBackgroundColor((R.color.white.toInt()))
+
+                    }else{
+                        esFavorito = true
+                        perrito?.favorito = 1.toByte()
+                        btnFav.setBackgroundColor(R.color.colorTintRed.toInt())
+                    }
+                }
         }
 
     }
